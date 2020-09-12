e8a4e8abd8dc24668c4b4cddd76d0be823ae56f1
Bethany Fitton <bethanyy.fitton@gmail.com>
2020-09-12 17:41:40 +0100

    Initial commit

---
+++ ceaser.js
@@ -0,0 +1,63 @@
+const alphabet = [
+    'A','B','C','D','E','F',
+    'G','H','I','J','K','L',
+    'M','N','O','P','Q','R',
+    'S','T','U','V','W','X',
+    'Y','Z'
+];
+
+
+function buttons() {
+    let plaintext = document.getElementById("plaintext").value;     //User input
+    let cleanPlaintext = (plaintext.trim()).toLowerCase();          //Clean user input
+    let encrypt = document.getElementById("encrypt");               //Encode radio button
+    let shift = document.getElementById("shift").value;             //Shift/Key amount
+  
+    returnValue = '';                                               //Default return value
+
+    //Check if Encrypt is pressed
+    if (encrypt.checked) {
+
+        for (var i = 0; i < cleanPlaintext.length; i++) {
+            returnValue += encryptFunc(cleanPlaintext.charAt(i), shift);
+        }
+
+    //Else, decrypt
+    } else{
+        for (var i = 0; i < cleanPlaintext.length; i++) {
+            returnValue += decryptFunc(cleanPlaintext.charAt(i), shift);
+        }
+    }
+
+    //Returns value to screen
+    output.innerHTML = returnValue;
+  }
+
+
+
+//Encrypt function
+function encryptFunc(char, shift) {
+    if (alphabet.includes(char.toUpperCase())) { 
+        position = alphabet.indexOf(char.toUpperCase());
+        newPosition = (Number(position) + Number(shift))%26;    //26 = Loop through again
+        return alphabet[newPosition];
+
+    } else { 
+        return char //If not in the alphabet array i.e. spaces
+    }
+}
+  
+
+//Decrypt function
+function decryptFunc(char, shift) {
+    if (alphabet.includes(char.toUpperCase())) { 
+        position = alphabet.indexOf(char.toUpperCase());
+        newPosition = (Number(position) - Number(shift))%26;    //Decrypt - Minus the shift
+        return alphabet[newPosition];
+
+    } else { 
+       return char 
+    }
+}
+
+//Bug with Decrypt - If X = C when encrypted, C shows undefined when decrypted. Is the loop not going backwards?
